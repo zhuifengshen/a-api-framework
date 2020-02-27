c081a5c181970053b79f2a6844d97e92030c7542
devin <zhangchuzhao@youmi.net>
2020-02-27 15:45:38 +0800

    fix: 修复sonar报错问题

--- meter/weapon3.py
+++ meter/weapon3.py
@@ -58,23 +58,23 @@ class Common(object):
             res = requests.put(url)
         return res
 
-        def delete(self, uri, params=None):
-            '''
-            封装你自己的delete方法，uri是访问路由，params是delete请求需要传递的参数，如果没有参数这里为空
-            :param uri: 访问路由
-            :param params: 传递参数，string类型，默认为None
-            :return: 此次访问的response
-            '''
-            url = self.url_root + uri
-            if params is not None:
-                # 如果有参数，那么通过delete方式访问对应的url，并将参数赋值给requests.delete默认参数data
-                # 返回request的Response结果，类型为requests的Response类型
-                res = requests.delete(url, data=params)
-            else:
-                # 如果无参数，访问方式如下
-                # 返回request的Response结果，类型为requests的Response类型
-                res = requests.delete(url)
-            return res
+    def delete(self, uri, params=None):
+        '''
+        封装你自己的delete方法，uri是访问路由，params是delete请求需要传递的参数，如果没有参数这里为空
+        :param uri: 访问路由
+        :param params: 传递参数，string类型，默认为None
+        :return: 此次访问的response
+        '''
+        url = self.url_root + uri
+        if params is not None:
+            # 如果有参数，那么通过delete方式访问对应的url，并将参数赋值给requests.delete默认参数data
+            # 返回request的Response结果，类型为requests的Response类型
+            res = requests.delete(url, data=params)
+        else:
+            # 如果无参数，访问方式如下
+            # 返回request的Response结果，类型为requests的Response类型
+            res = requests.delete(url)
+        return res
 
 
 if __name__ == "__main__":
